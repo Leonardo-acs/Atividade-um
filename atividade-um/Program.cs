a7ddc261df7ee1fe11bc6dc92589f1f789cdde3c
Leonardo-acs <leoacs2002@hotmail.com>
2021-09-15 13:33:03 -0300

    first commit

---
+++ atividade-um/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atividade_um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Leonardo!");
+        }
+    }
+}
